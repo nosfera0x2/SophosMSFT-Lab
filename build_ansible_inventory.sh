ab51b0510032f06609f1139728aa3a44cdaf19b3
nosfera0x2 <82817752+nosfera0x2@users.noreply.github.com>
2023-05-30 15:18:59 -0500

    Update build_ansible_inventory.sh

--- build_ansible_inventory.sh
+++ build_ansible_inventory.sh
@@ -25,16 +25,16 @@ DC_IP=$(echo "$TF_OUTPUT" | grep -E -o "dc_public_ip = \"([0-9]{1,3}[\.]){3}[0-9
 WIN10_IP=$(echo "$TF_OUTPUT" | grep -E -o "win10_public_ip = \"([0-9]{1,3}[\.]){3}[0-9]{1,3}\"" | cut -d '=' -f 2 | tr -d ' ' | tr -d '"')
 
 # Don't update unless there's default values in inventory.yml
-GREP_COUNT=$(grep -E -c 'x\.x\.x\.x|y\.y\.y\.y|z\.z\.z\.z' ../Ansible/inventory.yml)
+GREP_COUNT=$(grep -E -c 'x\.x\.x\.x|z\.z\.z\.z' ../Ansible/inventory.yml)
 if [ "$GREP_COUNT" -ne 3 ]; then
-  echo "This script is expecting the default values of x.x.x.x, y.y.y.y, and z.z.z.z for the dc, wef, and win10 hosts respectively in Ansible/inventory.yml."
+  echo "This script is expecting the default values of x.x.x.x, and z.z.z.z for the dc, and win10 hosts respectively in Ansible/inventory.yml."
   echo "You can restore the file to this state by running 'git checkout -- Ansible/inventory.yml'"
   echo "Rerun this script once that is complete."
   exit 1
 fi
 
 echo "Replacing the default values in DetectionLab/Azure/Ansible/inventory.yml..."
-sed -i.bak "s/x.x.x.x/$DC_IP/g; s/y.y.y.y/$WEF_IP/g; s/z.z.z.z/$WIN10_IP/g" ../Ansible/inventory.yml
+sed -i.bak "s/x.x.x.x/$DC_IP/g; s/z.z.z.z/$WIN10_IP/g" ../Ansible/inventory.yml
 
 echo "Displaying the updated inventory.yml below!"
 cat ../Ansible/inventory.yml
